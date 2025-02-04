60e22f31d66f0f0531baa0ec9016b75e06064fe6
Patrick <70688416+ItzPolarBearDK@users.noreply.github.com>
2025-02-04 09:32:00 +0100

    Revert "Configured models"

--- CommonResources/Models/Hotel.cs
+++ CommonResources/Models/Hotel.cs
@@ -4,21 +4,16 @@
     {
         public string Name { get; set; }
         public string Description { get; set; }
-        public string Country { get; set; }
-        public string Region { get; set; }
-        public string City { get; set; }
-        public string PostalCode { get; set; }
+        public string Location { get; set; }
         public List<Room> Rooms { get; set; }
         public List<User> Workers { get; set; }
         public List<Booking> Bookings { get; set; }
     }
-    public class CreateHotelDTO
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string Country { get; set; }
-        public string Region { get; set; }
-        public string City { get; set; }
-        public string PostalCode { get; set; }
-    }
+}
+
+public class CreateHotelDTO
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Location { get; set; }
 }
--- CommonResources/Models/Role.cs
+++ CommonResources/Models/Role.cs
@@ -3,6 +3,5 @@
     public class Role : Common
     {
         public string Name { get; set; }
-        public User User { get; set; }
     }
 }
