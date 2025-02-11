7e39a62e109d5e333bcc15b1463039a6b16c5346
Nikolaj Have <nhave06@gmail.com>
2025-02-11 14:40:27 +0100

    Auth Controller ready.

---
+++ HotelCommons/Models/RefreshToken.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace HotelsCommons.Models
+{
+    public class RefreshToken
+    {
+        public int Id { get; set; }
+        public string Token { get; set; }
+        public string UserId { get; set; }
+        public User User { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public bool IsRevoked { get; set; }
+    }
+
+    public class RefreshTokenDTO
+    {
+        [JsonPropertyName("refreshToken")]
+        public string RefreshToken { get; set; }
+    }
+}
