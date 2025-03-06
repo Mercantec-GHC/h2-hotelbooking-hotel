a08fda4f251337f88c88a1553ab231c3d26fbd88
nhave <nhave06@gmail.com>
2025-03-06 09:30:46 +0100

    Update ApiService.cs

--- HotelsWebApp/Services/ApiService.cs
+++ HotelsWebApp/Services/ApiService.cs
@@ -50,36 +50,18 @@ namespace HotelsWebApp.Services
        
         public async Task<List<BookingResult>> GetMyBookingsWithRooms()
         {
-            var bookingsWithRooms = new List<BookingResult>();
-            var savedToken = await _localStorage.GetItemAsync<string>("authToken");
-
-            if (!string.IsNullOrWhiteSpace(savedToken))
+            if (await IsAuthenticatedAsync())
             {
-                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", savedToken);
                 var response = await _httpClient.GetAsync($"api/Booking/GetMyBookings");
 
                 if (response.IsSuccessStatusCode)
                 {
-                    var bookings = JsonSerializer.Deserialize<List<BookingResult>>(await response.Content.ReadAsStringAsync(),
-                        new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-
-                    foreach (var booking in bookings)
-                    {
-                        var roomResponse = await _httpClient.GetAsync($"api/Room/{booking.RoomID}");
-                        if (roomResponse.IsSuccessStatusCode)
-                        {
-                            var room = JsonSerializer.Deserialize<RoomResult>(await roomResponse.Content.ReadAsStringAsync(),
-                                new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
-
-                            booking.RoomName = room.Name;
-                        }
-
-                        bookingsWithRooms.Add(booking);
-                    }
+                    var result = JsonSerializer.Deserialize<List<BookingResult>>(await response.Content.ReadAsStringAsync(), new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+                    return result;
                 }
             }
 
-            return bookingsWithRooms;
+            return new List<BookingResult>();;
         }
 
 
