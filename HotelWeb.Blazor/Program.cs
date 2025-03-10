e0b6f703f29d9011f25e863ccde7477c0b3f83cf
Nikolaj Have <nhave06@gmail.com>
2025-03-10 13:33:04 +0100

    New Project Test

---
+++ HotelWeb.Blazor/Program.cs
@@ -0,0 +1,88 @@
+
+using HotelWeb.Blazor.Auth;
+using HotelWeb.Blazor.Components;
+using Microsoft.AspNetCore.Authentication;
+using System.Security.Cryptography.X509Certificates;
+using HotelsRazorLibrary.Services;
+using HotelWeb.Blazor.Services;
+
+
+namespace HotelWeb.Blazor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            string? certPasswordFile = Environment.GetEnvironmentVariable("CERT_PASSWORD");
+
+            if (!string.IsNullOrEmpty(certPasswordFile) && File.Exists(certPasswordFile))
+            {
+                string certPassword = File.ReadAllText(certPasswordFile).Trim();
+
+                builder.WebHost.ConfigureKestrel(serverOptions =>
+                {
+                    serverOptions.ConfigureHttpsDefaults(httpsOptions =>
+                    {
+                        string? certPath = Environment.GetEnvironmentVariable("CERT_PATH");
+                        httpsOptions.ServerCertificate = new X509Certificate2(certPath, certPassword);
+                    });
+                });
+            }
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "jwt";
+                options.DefaultChallengeScheme = "jwt";
+            }).AddScheme<AuthenticationSchemeOptions, CustomAuthenticationHandler>("jwt", options => { });
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
+            builder.Services.AddHttpContextAccessor();
+
+            builder.Services.AddHotelLibrary();
+
+            builder.Services.AddScoped<ApiService>();
+            builder.Services.AddHttpClient<ApiService>(client =>
+            {
+                client.BaseAddress = new Uri("https://10.135.51.104:5101");
+                //client.BaseAddress = new Uri("https://10.135.71.51:5101");
+                //client.BaseAddress = new Uri("https://localhost:7090");
+            })
+            .ConfigurePrimaryHttpMessageHandler(() =>
+            {
+                return new HttpClientHandler
+                {
+                    ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+                };
+            });
+
+            var app = builder.Build();
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapRazorComponents<App>()
+                .AddInteractiveServerRenderMode();
+
+            app.Run();
+        }
+    }
+}
